39e556658bb5dccdcc9f98d2595f9540a49cd1da
NoHyeokPark <park92y@naver.com>
2025-08-08 00:26:15 +0900

    0808

---
+++ 27497.py
@@ -0,0 +1,19 @@
+from collections import deque
+n = int(input())
+q = deque()
+cur = []
+for _ in range(n):
+    a = input()
+    if a[0] == '1':
+        q.append(a[2])
+        cur.append(False)
+    elif a[0] == '2':
+        q.appendleft(a[2])
+        cur.append(True)
+    elif a[0] == '3':
+        if q:
+            if cur.pop():
+                q.popleft()
+            else:
+                q.pop()
+print(''.join(q))                
