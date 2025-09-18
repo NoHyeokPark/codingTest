9c7cd16b79b253bce3a01bab9b19276b91b2b0c1
NoHyeokPark <park92y@naver.com>
2025-09-19 00:21:43 +0900

    0919

---
+++ 1100.py
@@ -0,0 +1,8 @@
+chess = [input() for _ in range(8)]
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if chess[i][j] == 'F':
+            if (i + j)%2 == 0:
+                cnt +=1
+print(cnt)                
