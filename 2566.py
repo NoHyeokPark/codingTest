82b84c1c901772b9228e2219351a788d02d2daab
NoHyeokPark <park92y@naver.com>
2025-09-20 04:01:35 +0900

    0920

---
+++ 2566.py
@@ -0,0 +1,10 @@
+arr = [list(map(int, input().split())) for _ in range(9)]
+big = 0
+idx = (0,0)
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] > big:
+            big = arr[i][j]
+            idx = (i+1,j+1)
+print(big)
+print(f"{idx[0]} {idx[1]}")
