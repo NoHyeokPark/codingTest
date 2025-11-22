1f69a74c5b616dcbe155e2940855803c2795c1c0
NoHyeokPark <park92y@naver.com>
2025-11-23 00:18:56 +0900

    1123

---
+++ 1236.py
@@ -0,0 +1,9 @@
+y,x = map(int, input().split())
+arr = [[True]*y,[True]*x]
+
+for i in range(y):
+    for j, e in enumerate(input()):
+        if e == 'X':
+            arr[0][i] = False
+            arr[1][j] = False
+print(max(sum(arr[0]), sum(arr[1])))
